dac71c9b61c603b4c49fea1d14d6862e19d29fc9
jzheadley <zephyrinkg@gmail.com>
2016-11-30 23:35:41 -0500

    automated commit from script

--- assign6.sh
+++ assign6.sh
@@ -21,7 +21,8 @@ tr -c '[:alnum:]' '[\n*]' < $1 | sort | uniq -c | sort | head  -10
 # find words based on regex patterns
 # starts with "d" and ends with "d" can be upper or lower
 #sed '/d.*d//Ig'
-grep [dD].*[dD] $1
+echo "number of words that start and end with d or D"
+grep [dD].*[dD] $1 | wc 
 # starts with "A" or "a" and ends with anything
 
 # count numeric words
