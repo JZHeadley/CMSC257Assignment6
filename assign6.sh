a1e2ed7b7dab90179e4476b5b265509ba2694a02
jzheadley <zephyrinkg@gmail.com>
2016-11-30 22:53:20 -0500

    automated commit from script

---
+++ assign6.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#FILE= $(<$1)
+#echo "$FILE"
+
+# count number of lines
+echo "Number of lines in the file is "
+wc -l $1
+
+# count number of words
+echo "Number of words in the file is "
+wc -w $1
+
+# find most repetitive word
+echo "most repetitive word"
+tr -c '[:alnum:]' '[\n*]' < $1 | sort | uniq -c | sort -nr | head  -10
+
+# find least repetitive word
+echo "least repetitive word"
+tr -c '[:alnum:]' '[\n*]' < $1 | sort | uniq -c | sort | head  -10
+
+# find words based on regex patterns
+
+# automated committing to git repository
+git add -A .
+git commit -m "automated commit from script"
+git push -u origin master
