f4e6522439c1d14119b2681e277b151494b89842
jzheadley <zephyrinkg@gmail.com>
2016-11-30 23:42:03 -0500

    automated commit from script

--- assign6.sh
+++ assign6.sh
@@ -3,29 +3,29 @@
 #echo "$FILE"
 
 # count number of lines
-echo "Number of lines in the file is "
+printf "Number of lines in the file is "
 wc -l $1
 
 # count number of words
-echo "Number of words in the file is "
+printf "Number of words in the file is "
 wc -w $1
 
 # find most repetitive word
-echo "most repetitive word"
+printf "most repetitive word"
 tr -c '[:alnum:]' '[\n*]' < $1 | sort | uniq -c | sort -nr | head  -10
 
 # find least repetitive word
-echo "least repetitive word"
+printf "least repetitive word"
 tr -c '[:alnum:]' '[\n*]' < $1 | sort | uniq -c | sort | head  -10
 
 # find words based on regex patterns
 # starts with "d" and ends with "d" can be upper or lower
 #sed '/d.*d//Ig'
-echo "number of words that start and end with d or D"
+printf "number of words that start and end with d or D"
 grep -w [dD].*[dD] $1 #| wc 
 
 # starts with "A" or "a" and ends with anything
-echo "\nnumber of words that start with A or a"
+printf "\nnumber of words that start with A or a"
 grep -w [aA].* $1 #| wc
 
 # count numeric words
